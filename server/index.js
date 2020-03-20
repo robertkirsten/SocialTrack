255ba3cb9fdca567ef71da26030af1251c88bfcd
rohansaw <rohan.sawahn@hotmail.de>
2020-03-21 00:26:06 +0100

    Server post infection added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,33 @@
 var express = require('express');
 var app = express();
 
+let infectedPersons = []
+
 //Push benachrichtigung von hier schicken
 
 app.get('/', function (req, res) {
   res.send('You may be Infected :(');
 });
+
+app.post('/infected', function(req, res){
+  console.log("Added Person with uid: ", req.body.uid);
+
+  let data = {
+    uid: req.body.uid,
+    contacted: req.body.contacted
+  }
+
+  infectedPersons.push(data);
+  data.contected.forEach(user => {
+    alertUser(user);
+  });
+ 
+  res.send("");
+});
+
+function alertUser(){
+  //Send push Message to possibly infected user
+}
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!');
