b8ce55cfd7099257ac1e757a0ec9ace716999ac3
Robert Kirsten <mail@rkirsten.de>
2020-03-21 01:13:01 +0100

    robis changes

--- ContactReminder/screens/ScanContactPersonScreen.js
+++ ContactReminder/screens/ScanContactPersonScreen.js
@@ -11,7 +11,10 @@ import {
   TouchableOpacity,
   Button
 } from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import { Image, Platform, SectionList } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import * as uniqid from 'uniqid';
+import * as RNFS from 'react-native-fs';
 import * as WebBrowser from 'expo-web-browser';
 import * as Permissions from 'expo-permissions';
 import {BarCodeScanner} from 'expo-barcode-scanner';
@@ -82,6 +85,32 @@ export default class ScanContactPersonScreen extends Component {
   }
 
   render() {
+    console.log('Render page');
+    const path = RNFS.DocumentDirectoryPath +'/uniqid.txt';
+    let id;
+    RNFS.exists(path).then((exists) => {
+      if (exists) {
+        console.log('File already exists');
+        RNFS.readFile(path, 'utf8')
+            .then((res) => {
+              id = res;
+            })
+            .catch((err) => {
+              console.log(err.message);
+            });
+      } else {
+        id = uniqid();
+        RNFS.writeFile(path, id, 'uft8')
+            .then((success) => {
+              console.log('Unique id written into file');
+            })
+            .catch((err) => {
+              console.log(err.message);
+            });
+      }
+    });
+    //const id = uniqid;
+    console.log(id);
     const {
       scanning,
     } = this.state;
