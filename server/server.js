5056653dee387181fa8e38ed44c1e22035a4aa11
Jonas Schmidt <mail@schmidt-jonas.de>
2020-03-21 19:46:38 +0100

    changed server

--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ const routes = require('./routes');
 
 const app = express();
 
+// use request bodies
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+// mount all routes
 app.use(routes);
 
+// run server on localhost:3000
 app.listen(3000, () => console.log('App listening on port 3000!'));
