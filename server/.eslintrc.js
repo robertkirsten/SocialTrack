e593d6248603bebe27b858eece473d80c33d05a7
cnmicha <cnmicha@users.noreply.github.com>
2020-03-21 22:19:21 +0100

    Added linter for server

---
+++ server/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      "jsx": true
+    }
+  },
+  plugins: [
+    'react',
+    'react-native'
+  ],
+  rules: {
+    'max-len': 'off',
+    'linebreak-style': 'off',
+    'no-plusplus': 'off',
+    'no-console': 'off',
+    'no-use-before-define': 'off',
+    'no-new': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
