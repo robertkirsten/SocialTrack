8af29b2654bb921bf6023c46e3418e068e409db5
cnmicha <cnmicha@users.noreply.github.com>
2020-03-21 10:33:16 +0100

    UI

--- ContactReminder/screens/ScanContactPersonScreen.js
+++ ContactReminder/screens/ScanContactPersonScreen.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   Text,
   View,
@@ -12,6 +12,7 @@ import {BarCodeScanner} from 'expo-barcode-scanner';
 import QRCode from 'react-qr-code';
 import Constants from 'expo-constants';
 import {RepositoryFactory} from '../API/RepositoryFactory';
+
 const postUserId = RepositoryFactory.get('postUserId');
 
 
@@ -23,14 +24,14 @@ export default class ScanContactPersonScreen extends Component {
     scanning: false,
   };
 
-  async postData(id){
+  async postData(id) {
     postUserId.postUserId(id).then(res => {
       console.log("Connection succesfully added");
       console.log(res.data);
     })
-        .catch(error => {
-          console.log("Error occured: ", error);
-        });
+      .catch(error => {
+        console.log("Error occured: ", error);
+      });
 
   };
 
@@ -40,12 +41,12 @@ export default class ScanContactPersonScreen extends Component {
   }
 
   handleBarCodeScanned = ({type, data}) => {
-    this.setState({ scanning: false })
+    this.setState({scanning: false})
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   };
 
   barcodescan() {
-    const { hasCameraPermission } = this.state;
+    const {hasCameraPermission} = this.state;
 
     if (hasCameraPermission === null) {
       return <Text> Requesting for camera permission </Text>;
@@ -55,25 +56,22 @@ export default class ScanContactPersonScreen extends Component {
     }
 
     return (
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-      }}>
-        <BarCodeScanner 
-          onBarCodeScanned={ this.handleBarCodeScanned }
-          style={StyleSheet.absoluteFillObject}
+      <View style={styles.container}>
+        <Text style={{marginBottom: 10}}>Scanne den Barcode einer anderen Person, um euch zu verbinden.</Text>
+        <BarCodeScanner
+          onBarCodeScanned={this.handleBarCodeScanned}
+          style={{flex: 1, marginBottom: 20}}
         />
         <Button
-          title="Stop scanning"
-          onPress={ () => this.setState({scanning: false}) }
+          title="Zurück"
+          onPress={() => this.setState({scanning: false})}
         />
       </View>
     );
   }
 
   render() {
-    const { scanning } = this.state;
+    const {scanning} = this.state;
 
     if (scanning) {
       return this.barcodescan();
@@ -83,10 +81,10 @@ export default class ScanContactPersonScreen extends Component {
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
           <View style={styles.getStartedContainer}>
-            <Text>Dies ist dein persönlicher Code. Lass eine andere Person diesen Code scannen, um dich als Kontaktperson hinzuzufügen.</Text>
-            <Text>{deviceId}</Text>
+            <Text style={{marginBottom: 30}}>Dies ist dein persönlicher Code. Lass eine andere Person diesen Code
+              scannen, um dich als Kontaktperson hinzuzufügen.</Text>
             <QRCode value={deviceId}/>
-            <Text>Oder du scannst selbst den Code einer anderen Person.</Text>
+            <Text style={{marginTop: 50, marginBottom: 10}}>Oder du scannst selbst den Code einer anderen Person.</Text>
             <Button
               title="Anderen Code scannen"
               onPress={() => this.setState({scanning: true})}/>
