43834caceaf66e2db51881424210b722e027b25c
Jonas Schmidt <mail@schmidt-jonas.de>
2020-03-21 21:56:36 +0100

    refactor

--- ContactReminder/API/apiCalls/contactedUsersRepository.js
+++ ContactReminder/API/apiCalls/contactedUsersRepository.js
@@ -3,17 +3,13 @@ import Repository from "../repository";
 const resource = "/contacted";
 
 export default ({
-    postcontactedUsers(userId1, userId2)
-    {
-        return Repository.post(`${resource}`, {
-                id1: `${userId1}`,
-                id2: `${userId2}`,
-            }
-        )
+    postcontactedUsers(userId1, userId2) {
+        return Repository.post(resource, {
+            id1: userId1,
+            id2: userId2,
+        })
     },
     getUsers(userId){
-        //returns an array of persons which user contacted
         return Repository.get(`${resource}?id=${userId}`)
-
     }
 });
